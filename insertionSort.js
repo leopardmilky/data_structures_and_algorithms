cab3602885ba91742c8ede0f46d74f57e3f71a74
leopardmilky <matthew.lee0619@gmail.com>
2024-04-15 12:41:26 +0900

    bubble, insertion, merge

--- insertionSort.js
+++ insertionSort.js
@@ -1,15 +1,24 @@
-// function insertionSort(arr) {
-//     for(let i = 1; i < arr.length; i++) {
-//         let currentVal = arr[i];
-//         for(let j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
-//             arr[j+1] = arr[j];
-//         }
-//         arr[j+1] = currentVal;
-//     }
-//     return arr;
-// }
+/*
+삽입 정렬
+: 자신의 앞의 원소들과 비교해가며 자신의 위치를 찾아감.
+- 시작은 두번째 원소부터 시작함.
+- 자신의 앞의 원소들과 하나씩 비교하며 진행. 두번째는 첫번째와 비교, 세번째는 두번째, 첫번째와 비교 .... 이런식
+- 자신의 위치를 찾으면 그 위치에 원소를 넣기 위해 원소를 한칸씩 이동시킴.
+*/
 
-// insertionSort([4,2,7,34,1,8,0])
+
+function insertionSort(arr) {
+    for(let i = 1; i < arr.length; i++) {
+        let currentVal = arr[i];
+        for(let j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+            arr[j+1] = arr[j];
+        }
+        arr[j+1] = currentVal;
+    }
+    return arr;
+}
+
+insertionSort([4,2,7,34,1,8,0])
 
 function insertionSort2(arr) {
     for(let i = 1; i < arr.length; i++) {
--- mergeSort.js
+++ mergeSort.js
@@ -5,14 +5,12 @@
 - 2개씩 나누어진 부분 리스트들을 정렬하고 결합하는 과정을 반복하여 하나로 만든다. 
 */
 
-function merge(arr1, arr2) {
-    // console.log("arr1: ", arr1);
-    // console.log("arr2: ", arr2);
-    
+function merge(arr1, arr2) {    // 분할한 부분 리스트 원소끼리 비교하여 올바르게 정렬.
     let results = [];
     let i = 0;
     let j = 0;
-    while(i < arr1.length && j < arr2.length) {
+
+    while(i < arr1.length && j < arr2.length) { // 두 개의 부분 리스트의 각 원소끼리 비교
         if(arr2[j] > arr1[i]) {
             results.push(arr1[i]);
             i++;
@@ -21,27 +19,26 @@ function merge(arr1, arr2) {
             j++;
         }
     }
-    while(i < arr1.length) {
+
+    while(i < arr1.length) {    // 나머지 원소들 삽입
         results.push(arr1[i]);
         i++;
     }
-    while(j < arr2.length) {
+    
+    while(j < arr2.length) {    // 나머지 원소들 삽입
         results.push(arr2[j]);
         j++;
     }
-    console.log("results: ", results);
+
     return results
 }
 
-
 function mergeSort(arr) {   // 리스트 분할 함수.
-    console.log("arr: ", arr);
     if(arr.length <= 1) return arr;
     let mid = Math.floor(arr.length / 2);
     let left = mergeSort(arr.slice(0, mid));    // arr의 0번째 부터 mid전까지 반복 분할.
     let right = mergeSort(arr.slice(mid));      // arr의 mid부터 끝까지 반복 분할.
-    console.log("left: ", left);
-    console.log("right: ", right);
+
     return merge(left, right);
 }
 
