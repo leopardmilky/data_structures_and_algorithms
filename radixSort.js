9c2a327447d6f6aacd7eaa109f37ce71dbcdb475
leopardmilky <matthew.lee0619@gmail.com>
2024-04-27 00:08:53 +0900

    quick sort, selection sort

--- radixSort.js
+++ radixSort.js
@@ -1,3 +1,10 @@
+/*
+기수 정렬(radix sort)
+: 자릿수를 기준으로 정렬하는 알고리즘으로 값들 간의 비교를 통해 정렬하는 것과는 다른 방식.
+
+- 시간 복잡도가 O(n)이지만 메모리를 그만큼 많이 사용해야함.
+*/
+
 function getDigit(num, i) { // 자릿수 숫자 알아내기. (234,0 -> 4 | 234,1 -> 3)
     return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
 }
@@ -16,14 +23,16 @@ function mostDigits(nums) { // 배열에서 가장 높은 자릿수 알아내기
 }
 
 function radixSort(nums) {
-    let maxDigitCount = mostDigits(nums);
-    for(let k = 0; k < maxDigitCount; k++) {
-        let digitBuckets = Array.from({length: 10}, () => []);
-        for(let i = 0; i < nums.length; i++) {
-            let digit = getDigit(nums[i], k);
-            digitBuckets[digit].push(nums[i]);
+    let maxDigitCount = mostDigits(nums);   // 1. 가장 높은 자릿수를 찾는다.
+    for(let k = 0; k < maxDigitCount; k++) {    // 2. 가장 높은 자릿수 만큼 반복해야함.
+        let digitBuckets = Array.from({length: 10}, () => []);  // 3. 각 자릿수에 사용할 0~9까지의 버킷을 만든다.(배열 안에 배열10개)
+        for(let i = 0; i < nums.length; i++) {  // 4. 배열 길이 만큼 반복.
+            let digit = getDigit(nums[i], k);   // 5. 배열의 각 원소들의 자릿수 숫자를 구한다.
+            digitBuckets[digit].push(nums[i]);  // 6. 각 자리수에 맞는 배열에 원소 삽입.(ex. 일의 자리 숫자가 2이면, digitBuckets[2])
         }
-        nums = [].concat(...digitBuckets);
+        nums = [].concat(...digitBuckets);  // 7. 각 배열들을 합친다. 그리고 반복...
     }
     return nums;
 }
+
+radixSort([32,2,4,65,44,5454,333]);
--- selectionSort.js
+++ selectionSort.js
@@ -1,14 +1,24 @@
+/*
+선택 정렬(selection sort) 
+: 선택 정렬은 정렬되지 않은 데이터들에 대해 가장 작은 데이터를 찾아 가장 앞의 데이터와 교환해나가는 방식.
+
+- 주어진 리스트에서 최솟값을 찾는다.
+- 그 값을 맨 앞에 위치한 값과 바꾼다.
+- 맨 처음 위치를 뺀 나머지 리스트를 같은 방법으로 교체한다.
+- 원소가 하나만 남을 때까지 반복한다.
+*/
+
 function selectionSort(arr) {
     const swap = (arr, idx1, idx2) => ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]);
 
-    for(let i = 0; i < arr.length; i++) {
+    for(let i = 0; i < arr.length; i++) {   // 첫번째 원소를 가장 작은 원소라고 가정하고 순회.(순회가 끝나면 그 다음 원소 진행)
         let lowest = i;
-        for(let j = i + 1; j < arr.length; j++) {
+        for(let j = i + 1; j < arr.length; j++) {   // 그 다음 원소와 비교하면서 가장 작은 원소를 갱신.
             if(arr[lowest] > arr[j]) {
                 lowest = j;
             }
         }
-        if(i !== lowest) swap(arr, i, lowest);
+        if(i !== lowest) swap(arr, i, lowest);  // 리스트 끝까지 순회하면 lowest에 가장 작은 원소만 남음. 위치 스왑을 진행.
     }
     return arr;
 }
