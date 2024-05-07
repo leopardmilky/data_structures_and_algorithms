07a504b44a48d4e86680118f9851ee49aecade43
leopardmilky <matthew.lee0619@gmail.com>
2024-05-07 22:57:41 +0900

    doublyLinkedList

--- singlyLinkedLists.js
+++ singlyLinkedLists.js
@@ -82,25 +82,25 @@ class SinglyLinkedList {
     }
     insert(index, val) {    // 해당 위치에 노드 추가
         if(index < 0 || index > this.length) return false;
-        if(index === this.length) return !!this.push(val);  // !! -> 불리언 형식으로 결과를 받기위해 사용.
-        if(index === 0) return !!this.unshift(val);
+        if(index === this.length) return !!this.push(val);  // !! -> 불리언 형식으로 결과를 받기위해 사용. length와 index가 같으면 맨 끝 노드이므로 push로 추가.
+        if(index === 0) return !!this.unshift(val);         // index가 0이면 맨 앞 노드이므로 unshift로 추가.
 
-        let newNode = new Node(val);
-        let prev = this.get(index - 1);
-        let temp = prev.next;
-        prev.next = newNode;
-        newNode.next = temp;
+        let newNode = new Node(val);        // 추가 하려는 노드 생성.
+        let prev = this.get(index - 1);     // 추가 하려는 위치 바로 전 노드 검색 (next를 변경해야함)
+        let temp = prev.next;               // 추가 하려는 위치에 있는 노드를 임시변수에 저장(노드를 추가한 후 다음 노드가 되어야함.)
+        prev.next = newNode;                // 추가 하려는 위치 바로 전 노드의 next에 추가.
+        newNode.next = temp;                // 임시변수를 추가된 노드 next에 추가.
         this.length++;
         return true;
     }
     remove(index) { // 해당 위치의 노드 제거
         if(index < 0 || index >= this.length) return undefined;
-        if(index === 0) return this.shift();
-        if(index === this.length - 1) return this.pop();
+        if(index === 0) return this.shift();                // index가 0이면 맨 앞이므로 shift로 노드 제거.
+        if(index === this.length - 1) return this.pop();    // index가 length-1과 같으면 맨 뒤이므로 pop으로 제거.
 
-        let previousNode = this.get(index - 1);
-        let removed = previousNode.next;
-        previousNode.next = removed.next;
+        let previousNode = this.get(index - 1);             // 제거하려는 바로 전 노드 찾기.
+        let removed = previousNode.next;                    // 바로 전 노드의 next(제거하려는 노드)를 변수에 저장
+        previousNode.next = removed.next;                   // 제거 하려는 노드의 바로 전 노드next에 제거한 다음 노드next를 연결.
         this.length--;
         return removed;
     }
